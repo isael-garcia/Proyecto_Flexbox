294dac3a7c40ccc029cfea858391917e061c2dbf
Isael Garcia <isaelgarcia1997@gmail.com>
2021-02-09 13:51:03 -0600

    proyecto entero 1

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css(){
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserlist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'expanded', 
+        }))
+        .pipe(gulp.dest('css'));
+}
+function watchFiles(){
+    gulp.watch('scss/*.scss', css);
+
+}
+
+gulp.task('css', css);
+gulp.task('watch', watchFiles);
